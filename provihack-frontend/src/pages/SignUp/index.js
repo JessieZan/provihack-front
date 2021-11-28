c34743926c5ea4b6e9263ecc22047cdf421e4ba4
Quezia <queziaamenezes@gmail.com>
2021-11-28 15:54:53 -0300

    estilos do cadastro

--- provihack-frontend/src/pages/SignUp/index.js
+++ provihack-frontend/src/pages/SignUp/index.js
@@ -3,6 +3,11 @@ import InputMask from 'react-input-mask';
 import { Link, useNavigate } from 'react-router-dom';
 import arrowLeft from './assets/arrowLeft.svg';
 import './style.css';
+import userIcon from './assets/user.svg';
+import atSign from './assets/at-sign.svg';
+import phoneIcon from './assets/phone.svg';
+import lockIcon from './assets/lock.svg';
+import ActionButton from '../../components/ActionButton';
 
 const SignUp = () => {
   const navigate = useNavigate();
@@ -30,47 +35,69 @@ const SignUp = () => {
 
   return (
     <div className='container-signup'>
-      <button className='arrow-left'>
-        <img src={arrowLeft} alt='' />
-      </button>
-      <h1>Cadastre-se</h1>
+      <div className='arrow-and-title'>
+        <button className='arrow-left'>
+          <img src={arrowLeft} alt='' />
+        </button>
+        <p>Cadastro de Usuário</p>
+      </div>
+
       <form onSubmit={handleSubmit} action='submit'>
-        <input
-          id='name'
-          type='text'
-          placeholder='Nome'
-          onChange={(event) => handleChange(event)}
-          value={form.name}
-        />
+        <div>
+          <img className='userIcon' src={userIcon} alt='' />
+          <input
+            id='name'
+            type='text'
+            placeholder='Nome Social Completo'
+            onChange={(event) => handleChange(event)}
+            value={form.name}
+          />
+        </div>
+        <div>
+          <img src={atSign} alt='' />
+          <input
+            id='email'
+            type='text'
+            placeholder='E-mail'
+            onChange={(event) => handleChange(event)}
+            value={form.email}
+          />
+        </div>
+        <div>
+          <img src={phoneIcon} alt='' />
+          <InputMask
+            id='phone'
+            mask='(99)99999-9999'
+            type='text'
+            placeholder='Telefone'
+            onChange={(event) => handleChange(event)}
+            value={form.phone}
+          />
+        </div>
+        <div>
+          <img src={lockIcon} alt='' />
+          <input
+            id='password'
+            type='password'
+            placeholder='Senha'
+            onChange={(event) => handleChange(event)}
+            value={form.password}
+          />
+          <span>Sua senha deve conter no mínimo 5 caracteres</span>
+        </div>
+        <div>
+          <img src={lockIcon} alt='' />
+          <input
+            id='password'
+            type='password'
+            placeholder='Confirmar senha'
+            onChange={(event) => handleChange(event)}
+            value={form.password}
+          />
+        </div>
+        <div />
 
-        <input
-          id='email'
-          type='text'
-          placeholder='E-mail'
-          onChange={(event) => handleChange(event)}
-          value={form.email}
-        />
-
-        <input
-          id='password'
-          type='password'
-          placeholder='Senha'
-          onChange={(event) => handleChange(event)}
-          value={form.password}
-        />
-
-        <InputMask
-          id='phone'
-          mask='(99)99999-9999'
-          type='text'
-          placeholder='Telefone'
-          onChange={(event) => handleChange(event)}
-          value={form.phone}
-        />
-
-        <button className='btn-signup'>Cadastrar</button>
-        <span>Já tem cadastro?</span>
-        <Link to='/login'>Clique aqui!</Link>
+        <ActionButton>Cadastrar</ActionButton>
       </form>
     </div>
   );
