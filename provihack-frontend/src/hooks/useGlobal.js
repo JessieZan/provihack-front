605076b263042c138b0a6b6f6181c1d3d43b5212
Quezia <queziaamenezes@gmail.com>
2021-11-28 13:42:31 -0300

    correção

--- provihack-frontend/src/hooks/useGlobal.js
+++ provihack-frontend/src/hooks/useGlobal.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import GlobalContext from '../context/GlobalContext';
+import GlobalContext from '../contexts/GlobalContext';
 
 const useGlobal = () => {
   return useContext(GlobalContext);
