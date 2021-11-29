35aafdb82b9594a0bc5608f5f8b0e57ef26dbc0e
Quezia <queziaamenezes@gmail.com>
2021-11-28 22:44:23 -0300

    adicionando seta na pagina mentorias

--- provihack-frontend/src/pages/Home/index.js
+++ provihack-frontend/src/pages/Home/index.js
@@ -1,20 +1,20 @@
-import "./style.css";
-import { Link, useNavigate } from "react-router-dom";
-import CardHome from "../../components/CardHome";
-import theme from "./theme1.js";
-import { ThemeProvider } from "@material-ui/core";
-import { Typography, List, Avatar } from "@mui/material";
-import getServices from "../../utils/getServices";
-import { useEffect } from "react";
-import useGlobal from "../../hooks/useGlobal";
-import { motion } from "framer-motion";
-import homeImage from "./assets/homeImage.svg";
-import ActionButton from "../../components/ActionButton";
+import './style.css';
+import { Link, useNavigate } from 'react-router-dom';
+import CardHome from '../../components/CardHome';
+import theme from './theme1.js';
+import { ThemeProvider } from '@material-ui/core';
+import { Typography, List, Avatar } from '@mui/material';
+import getServices from '../../utils/getServices';
+import { useEffect } from 'react';
+import useGlobal from '../../hooks/useGlobal';
+import { motion } from 'framer-motion';
+import homeImage from './assets/homeImage.svg';
+import arrowBack from '../SignUp/assets/arrowLeft.svg';
 
 function Results({ id, nome, areas, categoria }) {
   useEffect(() => {
     async function getData() {
-      const data = await getServices("cursos");
+      const data = await getServices('cursos');
       console.log(data);
     }
     getData();
@@ -30,49 +30,48 @@ function Results({ id, nome, areas, categoria }) {
   console.log(token);
 
   const handleLogout = () => {
-    navigate("/login");
+    navigate('/login');
     removeToken();
   };
 
   return (
     <motion.div
-      initial="hidden"
-      animate="visible"
+      initial='hidden'
+      animate='visible'
       variants={variants}
-      className="results"
-    >
+      className='results'>
       <ThemeProvider theme={theme}>
-        <div className="header">
+        <div className='header'>
           <img
-            src="arrowBack.svg"
-            alt="Botão de Retorno"
+            src='arrowBack.svg'
+            alt='Botão de Retorno'
             onClick={() => handleLogout()}
           />
           Home <Avatar />
         </div>
-        <div className="content">
-          <h2 className="titulo">Projeto TRANScender</h2>
+        <div className='content'>
+          <h2 className='titulo'>Projeto TRANScender</h2>
           <span>
             Rede de apoio a pessoas trans na qualificação para o mercado de
             trabalho.
           </span>
         </div>
-        <img className="home-image" src={homeImage} alt="" />
+        <img className='home-image' src={homeImage} alt='' />
         <p>
           Aqui você pode encontrar serviços de mentoria, cursos, assessoria
           jurídica e apoio psicológico.
         </p>
-        <div className="welcome-title">
+        <div className='welcome-title'>
           <h2>Legal te ver por aqui! Escolha uma trilha:</h2>
         </div>
-        <div className="cards">
-          <CardHome titulo="Mentorias" categoria="mentorias" />
-          <CardHome titulo="Cursos" categoria="cursos" />
-          <CardHome titulo="Apoio Psicológico" categoria="juridico" />
-          <CardHome titulo="Acessoria Jurídica" categoria="psicologico" />
+        <div className='cards'>
+          <CardHome titulo='Mentorias' categoria='mentorias' />
+          <CardHome titulo='Cursos' categoria='cursos' />
+          <CardHome titulo='Apoio Psicológico' categoria='juridico' />
+          <CardHome titulo='Acessoria Jurídica' categoria='psicologico' />
         </div>
 
-        <Link to="/servicos/:id"></Link>
+        <Link to='/servicos/:id'></Link>
       </ThemeProvider>
     </motion.div>
   );
--- provihack-frontend/src/pages/SearchResults/index.js
+++ provihack-frontend/src/pages/SearchResults/index.js
@@ -1,40 +1,47 @@
-import './style.css'
-import { Link } from 'react-router-dom'
-import Card from '../../components/Card'
-import getServices from '../../utils/getServices'
-import { useEffect } from 'react'
-import useGlobal from '../../hooks/useGlobal'
+import './style.css';
+import { Link } from 'react-router-dom';
+import Card from '../../components/Card';
+import getServices from '../../utils/getServices';
+import { useEffect } from 'react';
+import useGlobal from '../../hooks/useGlobal';
+import arrowBack from '../SignUp/assets/arrowLeft.svg';
+import { useNavigate } from 'react-router-dom';
 
 function Results({ nome, areas, categoria }) {
-  const { token, services, setServices, category } = useGlobal()
-
+  const { token, services, setServices, category } = useGlobal();
+  const navigate = useNavigate();
   useEffect(() => {
     async function getData() {
-      const data = await getServices(category, token)
-      console.log(data)
-      setServices(data)
+      const data = await getServices(category, token);
+      console.log(data);
+      setServices(data);
     }
-    getData()
-  }, [])
+    getData();
+  }, []);
 
   return (
-    <div className="resultados">
-      <div className='titulo1'>Mentorias</div>
-      <div className="conteudo">
-        <h2 className="titulo"> Encontre seu mentor</h2>
+    <div className='resultados'>
+      <div className='titulo1'>
+        <button onClick={() => navigate('/home')}>
+          <img src={arrowBack} alt='' />
+        </button>{' '}
+        Mentorias
+      </div>
+      <div className='conteudo'>
+        <h2 className='titulo'> Encontre seu mentor</h2>
         <span>1. Encontre o mentor ideal.</span>
         <span>2. Agende um dia e horário.</span>
         <span>3. Realize o atendimento.</span>
         <h4>Pronto! Simples e grátis.</h4>
       </div>
 
-      <div className="cardes">
+      <div className='cardes'>
         {services.map((item) => (
           <Card item={item} />
         ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Results
+export default Results;
